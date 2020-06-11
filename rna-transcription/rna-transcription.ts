1b7440d44be33babc6bf186c250de79d71eb42db
Clara Gaset <claragaset4@gmail.com>
2020-06-11 12:08:49 +0200

    First iteration rna-transcriptor

--- rna-transcription/rna-transcription.ts
+++ rna-transcription/rna-transcription.ts
@@ -1,6 +1,23 @@
+
+const NucleotidesComplements = {
+    'A': 'U',
+    'C': 'G',
+    'G': 'C',
+    'T': 'A'
+}
+
+type Nucleotide = keyof typeof NucleotidesComplements
+
 class Transcriptor {
-    toRna( /* Parameters go here */ ) {
-        // Your code here
+
+    toRna(nucleotides: string) {
+      const result = [...nucleotides].reduce((acc, val) => acc += this.transcribe(val), '')
+      return result
+    }
+
+    private transcribe = (value: string): string => {
+      if (value !== 'A' && value !== 'C' && value !== 'G' && value !== 'T') throw new Error('Invalid input DNA.')
+      return NucleotidesComplements[value]
     }
 }
 
