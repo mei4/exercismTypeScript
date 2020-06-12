1e683c68229a65fde4ab9e55f8af166ce3879d10
Clara Gaset <claragaset4@gmail.com>
2020-06-12 16:46:48 +0200

    Extracts object creation to function

--- leap/leap.ts
+++ leap/leap.ts
@@ -1,9 +1,13 @@
 function isLeapYear(year: number) {
-    const yearEnhanced = {
-        isDivisibleBy: (n: number) => { return year%n === 0 }
-    }
+    const yearEnhanced = enhanceYear(year)
     
     return yearEnhanced.isDivisibleBy(4) && (!yearEnhanced.isDivisibleBy(100) || yearEnhanced.isDivisibleBy(400))
+}
+
+export function enhanceYear(year: number) {
+    return { 
+		isDivisibleBy: (n: number) => { return year%n === 0 } 
+	}
 }
 
 export default isLeapYear
