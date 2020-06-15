9d2840db49c26311a84c49905e1c3f8b0bb3c229
Clara Gaset <claragaset4@gmail.com>
2020-06-15 11:37:09 +0200

    Adds triangle solution

--- triangle/triangle.ts
+++ triangle/triangle.ts
@@ -1,12 +1,30 @@
 export default class Triangle {
 
-    sides: number[]
+	sides: number[]
 
-    constructor(...sides: number[]) {
-        this.sides = sides
-    }
+	constructor(...sides: number[]) {
+		this.sides = sides
+	}
 
-    kind() {
-        return ""
-    }
+	kind(): string {
+		if (!this.isTriangle()) throw new Error('Invalid input sides')
+		if (this.sides.every(side => side === this.sides[0])) return "equilateral"
+		if (this.isIsosceles()) return "isosceles"
+		return "scalene"
+	}
+
+	private isTriangle() {
+		const [side1, side2, side3] = this.sides
+		const isUnequal = side1 + side2 < side3 || side1 + side3 < side2 || side2 + side3 < side1
+		
+		return this.sides.every(side => side !== 0) && !isUnequal
+	}
+
+	private isIsosceles() {
+		const [side1, side2, side3] = this.sides
+		if (side1 === side2) return true
+		if (side1 === side3) return true
+		if (side2 === side3) return true
+		return false
+	}
 }
