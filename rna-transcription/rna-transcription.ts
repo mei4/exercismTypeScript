03a453b361b295c1c892f1ddd0dfda81f56007ea
Clara Gaset <claragaset4@gmail.com>
2020-06-19 09:25:06 +0200

    Adds DNAtoRNA type

--- rna-transcription/rna-transcription.ts
+++ rna-transcription/rna-transcription.ts
@@ -1,15 +1,16 @@
 import { off } from "process"
 
-const DNAtoRNA = {
+const DNAtoRNA: Record<DNA,RNA> = {
   'A': 'U',
   'C': 'G',
   'G': 'C',
   'T': 'A'
 } as const
 
-type DNA = keyof typeof DNAtoRNA
 
-type RNA = typeof DNAtoRNA[DNA]
+type DNA = 'A' | 'C' | 'G' | 'T'
+
+type RNA = 'U' | 'G' | 'C' | 'A'
 
 
 class Transcriptor {
@@ -17,9 +18,7 @@ class Transcriptor {
   toRna(nucleotides: string): string {
     if (nucleotides.match(new RegExp('[^A|G|T|C]'))) throw new Error('Invalid input DNA.')
     
-    const replacer = (match: string): RNA => { return DNAtoRNA[match as DNA] }
-    
-    return nucleotides.replace(/A|G|T|C/g, replacer)
+    return nucleotides.replace(/A|G|T|C/g, (match) => { return DNAtoRNA[match as DNA] })
   }
 }
 
